2e70d88c8da44b207deb9f380fa6785766df88ce
young <jinyoung7165@gmail.com>
2023-08-08 15:57:39 +0900

    [ADD] item2: builder

---
+++ effectiveJava/src/ch1/item2/builder/common/Nutrition.java
@@ -0,0 +1,41 @@
+package ch1.item2.builder.common;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class Nutrition { // final -> 생성자 주입 받을 것
+    private final int calorie; // 필수
+    private final int sodium; // 필수
+    private final int protein;
+
+    private Nutrition(Builder builder) {
+        calorie = builder.calorie;
+        sodium = builder.sodium;
+        protein = builder.protein;
+    }
+
+    public static Builder builder(int calorie, int sodium) {
+        return new Builder(calorie, sodium);
+    }
+
+    public static class Builder {
+        private final int calorie; // 필수
+        private final int sodium; // 필수
+        private int protein; // optional -> final 아님
+
+        public Builder(int calorie, int sodium) {
+             this.calorie = calorie;
+             this.sodium = sodium;
+        }
+
+        public Builder protein(int protein) {
+            this.protein = protein;
+            return this; // method chaining 위함 -> 속성 언제든 추가 가능
+        }
+
+        public Nutrition build() {
+            return new Nutrition(this);
+        }
+    }
+}
